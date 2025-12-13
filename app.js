633955de8641f1210b6b5b23547eb312bfe3f6aa
Rahul Mahendrakar <rahulm31304@gmail.com>
2025-12-13 14:57:08 +0530

    build check 2

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 import express from "express";
 const a = express();
-a.get("/", (_, res) => res.send("Test app is live"));
+a.get("/", (_, res) => res.send("Test app live"));
 a.listen(8000, () => console.log("Test app running...."));
