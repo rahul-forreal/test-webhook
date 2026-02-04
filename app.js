4d321d7718a38a486555b9f62a9dded1819deb06
Rahul Mahendrakar <rahulm31304@gmail.com>
2026-02-05 00:27:08 +0530

    test full deployment

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 import express from "express";
 const a = express();
-a.get("/", (_, res) => res.send("Test app live!!"));
+a.get("/", (_, res) => res.send("Test app is finally live!!"));
 a.listen(8000, () => console.log("Test app running..."));
