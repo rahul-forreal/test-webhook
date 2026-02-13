0fc66df9bfff6ba6f9ef677a6569c37d0c07da19
Rahul Mahendrakar <rahulm31304@gmail.com>
2026-02-13 16:31:15 +0530

    check deploy

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
 const a = express();
 const port=process.env.PORT || 8000;
-a.get("/", (_, res) => res.send("DEPLOY CHECK, ALL GOOD!"));
+a.get("/", (_, res) => res.send("DEPLOY CHECK, ALL GOOD!!"));
 a.listen(port, () => console.log("Test app running..."));
